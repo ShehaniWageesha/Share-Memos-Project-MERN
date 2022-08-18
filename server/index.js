229cfd808c852c0957ef86354327355c56da9727
Shehani Wageesha <66903669+ShehaniWageesha@users.noreply.github.com>
2022-08-18 13:23:50 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://admin:admin@cluster1.4y8vp.mongodb.net/memo_db?retryWrites=true&w=majority';
+const CONNECTION_URL = 'your_mongodb_url';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
